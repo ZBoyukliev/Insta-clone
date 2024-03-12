e4859f8d01d7264527451484d353cb1cadf2fc1b
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-12 12:35:22 +0200

    common changes

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,9 +1,31 @@
+import { Box, Container, Flex, Image, VStack } from "@chakra-ui/react"
+import AuthForm from "../../components/AuthForm/AuthForm"
 
 
 const AuthPage = () => {
-  return (
-    <div>AuthPage</div>
-  )
+    return (
+        <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+            <Container maxW={"container.md"} padding={0}>
+                {/* Left-Hand Side  */}
+                <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+                <Box display={{ base: "none", md: "block" }}>
+                    <Image src="/auth.png" h={650} alt="Phone png" />
+                </Box>
+                {/* Right-Hand Side  */}
+                <VStack spacing={4} align={"stretch"}>
+                    <AuthForm />
+                    <Box textAlign={"center"}>
+                        Get the App.
+                    </Box>
+                    <Flex gap={5} justifyContent={"center"}>
+                        <Image src="/playstore.png" h={10} alt="Playstore logo"/>
+                        <Image src="/microsoft.png" h={10} alt="Microsoft logo"/>
+                    </Flex>
+                </VStack>
+                </Flex>
+            </Container>
+        </Flex>
+    )
 }
 
 export default AuthPage
