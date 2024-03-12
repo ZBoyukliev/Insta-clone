8e522af15d2febd689d05a27eb98b45374c46290
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-12 11:44:15 +0200

    change bg color to black

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+import type { GlobalStyleProps } from '@chakra-ui/theme-tools';
+
+const styles = {
+	global: (props: GlobalStyleProps) => ({
+		body: {
+			bg: mode("gray.100", "black")(props),
+			color: mode("gray.800", "whiteAlpha.900")(props),
+		},
+	}),
+};
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+const theme = extendTheme({ config, styles })
+
+export default theme
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
