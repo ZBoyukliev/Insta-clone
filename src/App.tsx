085cd0d92a4fa71b6667006b3b594922239a522c
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-12 12:03:42 +0200

    create home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Button } from "@chakra-ui/react"
-
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage/HomePage"
 
 function App() {
 
   return (
     <>
-      <Button>Hello world</Button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </>
   )
 }
