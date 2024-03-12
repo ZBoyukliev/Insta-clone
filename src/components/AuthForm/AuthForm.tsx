074c1dc81c13aec3dcb8ff0c8d296d665ccb3661
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-12 16:12:28 +0200

    style the Auth form

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,8 +1,34 @@
+import { Box, Button, Flex, Image, Input, Text, VStack } from "@chakra-ui/react"
+import { useState } from "react"
 
 const AuthForm = () => {
-  return (
-    <div>AuthForm</div>
-  )
+
+    const [isLogin, setIsLogin] = useState(true);
+
+    return (
+        <>
+            <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+                <VStack>
+                    <Image src="/logo.png" h={24} cursor={"pointer"} alt="Instagram logo" />
+                    <Input placeholder='Email' fontSize={14} type="email" />
+                    <Input placeholder='Password' fontSize={14} type="password" />
+                    {!isLogin && <Input placeholder='Confirm password' fontSize={14} type="password" />}
+                    <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={16}>
+                        {isLogin ? "Login" : "Sign up"}
+                    </Button>
+                    <Flex alignItems={"center"} justifyContent={"center"} my={4} w={"full"}>
+                        <Box flex={2} h={"1px"} bg={"gray.400"} />
+                        <Text mx={2} color={"white"}>OR</Text>
+                        <Box flex={2} h={"1px"} bg={"gray.400"} />
+                    </Flex>
+                    <Flex>
+                        <Image src="/google.png" h={5} alt="Google logo" cursor={"pointer"} />
+                        <Text mx={2}>Login with Google</Text>
+                    </Flex>
+                </VStack>
+            </Box>
+        </>
+    )
 }
 
 export default AuthForm
