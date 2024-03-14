62ce04f0e05bfb6f1089e97e32b6038ed3efc9b7
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-14 14:19:01 +0200

    create Notifications item in sidebar

--- src/components/Sidebar/SidebarItems.tsx
+++ src/components/Sidebar/SidebarItems.tsx
@@ -1,9 +1,13 @@
 import Home from "./items/Home";
+import Notifications from "./items/Notifications";
+import Search from "./items/Search";
 
 const SidebarItems = () => {
 	return (
 		<>
 			<Home />
+            <Search />
+            <Notifications />
 		</>
 	);
 };
