10162333b8ba26c1d88e47f4391c71471bd9ea04
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-14 11:29:24 +0200

    create sidebar items

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Link } from "@chakra-ui/react"
 import { Link as RouterLink } from "react-router-dom";
-import { InstagramLogo } from "../../assets/constants";
+import { InstagramLogo, InstagramMobileLogo } from "../../assets/constants";
+import SidebarItems from "./SidebarItems";
 
 
 const Sidebar = () => {
@@ -16,9 +17,26 @@ const Sidebar = () => {
             px={{ base: 2, md: 4 }}
         >
             <Flex direction={"column"} gap={10} w='full' height={"full"}>
-            <Link to={"/"} as={RouterLink} pl={2} display={{ base: "none", md: "block" }} cursor='pointer'>
-					<InstagramLogo />
-				</Link>
+                <Link to={"/"} as={RouterLink} pl={2} display={{ base: "none", md: "block" }} cursor='pointer'>
+                    <InstagramLogo />
+                </Link>
+                <Link
+                    to={"/"}
+                    as={RouterLink}
+                    p={2}
+                    display={{ base: "block", md: "none" }}
+                    borderRadius={6}
+                    _hover={{
+                        bg: "whiteAlpha.200",
+                    }}
+                    w={10}
+                    cursor='pointer'
+                >
+                    <InstagramMobileLogo />
+                </Link>
+                <Flex direction={"column"} gap={5} cursor={"pointer"}>
+					<SidebarItems />
+				</Flex>
             </Flex>
         </Box>
     )
