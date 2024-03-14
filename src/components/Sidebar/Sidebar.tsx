55c6e17265ec7339638247d46e5174fc13fba2ee
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-14 11:47:56 +0200

    create Logout item in sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,8 @@
-import { Box, Flex, Link } from "@chakra-ui/react"
+import { Box, Button, Flex, Link, Tooltip } from "@chakra-ui/react"
 import { Link as RouterLink } from "react-router-dom";
 import { InstagramLogo, InstagramMobileLogo } from "../../assets/constants";
 import SidebarItems from "./SidebarItems";
+import { BiLogOut } from "react-icons/bi";
 
 
 const Sidebar = () => {
@@ -37,6 +38,35 @@ const Sidebar = () => {
                 <Flex direction={"column"} gap={5} cursor={"pointer"}>
 					<SidebarItems />
 				</Flex>
+                {/* LOGOUT */}
+				<Tooltip
+					hasArrow
+					label={"Logout"}
+					placement='right'
+					ml={1}
+					openDelay={500}
+					display={{ base: "block", md: "none" }}
+				>
+					<Flex
+						alignItems={"center"}
+						gap={4}
+						_hover={{ bg: "whiteAlpha.400" }}
+						borderRadius={6}
+						p={2}
+						w={{ base: 10, md: "full" }}
+						mt={"auto"}
+						justifyContent={{ base: "center", md: "flex-start" }}
+					>
+						<BiLogOut size={25} />
+						<Button
+							display={{ base: "none", md: "block" }}
+							variant={"ghost"}
+							_hover={{ bg: "transparent" }}
+						>
+							Logout
+						</Button>
+					</Flex>
+				</Tooltip>
             </Flex>
         </Box>
     )
