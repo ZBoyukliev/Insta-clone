319593391eae7c25b78f99576a1007ab9f64981d
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-15 11:46:10 +0200

    style post footer post

--- src/components/FeedPosts/PostFooter.tsx
+++ src/components/FeedPosts/PostFooter.tsx
@@ -1,8 +1,46 @@
+import { Box, Flex,Text } from "@chakra-ui/react";
+import { CommentLogo, NotificationsLogo } from "../../assets/constants";
+
 
 const PostFooter = () => {
-  return (
-    <div>PostFooter</div>
-  )
-}
+	
 
-export default PostFooter
+	return (
+		<Box mb={10} marginTop={"auto"}>
+			<Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
+				<Box cursor={"pointer"} fontSize={18}>
+					 <NotificationsLogo /> 
+            {/* <UnlikeLogo /> */}
+				</Box>
+
+				<Box cursor={"pointer"} fontSize={18} >
+					<CommentLogo />
+				</Box>
+			</Flex>
+			<Text fontWeight={600} fontSize={"sm"}>
+			 likes
+			</Text>
+
+				<Text fontSize='12' color={"gray"}>
+					Posted time ago
+				</Text>
+
+				<>
+					<Text fontSize='sm' fontWeight={700}>
+						creator profile username
+						<Text as='span' fontWeight={400}>
+							post.caption
+						</Text>
+					</Text>
+						<Text fontSize='sm' color={"gray"} cursor={"pointer"} >
+							View all  comments
+						</Text>
+					{/* COMMENTS MODAL ONLY IN THE HOME PAGE */}
+					
+				</>
+
+		</Box>
+	);
+};
+
+export default PostFooter;
--- src/components/FeedPosts/PostHeader.tsx
+++ src/components/FeedPosts/PostHeader.tsx
@@ -2,10 +2,15 @@ import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
 const PostHeader = () => {
   return (
-    <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"} my={2}>
+    <Flex
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      w={"full"}
+      my={2}
+    >
       <Flex alignItems={"center"} gap={2}>
         <Avatar src="/img1.png" size={"sm"} />
-        <Flex fontSize={12} fontWeight={"bold"} gap='2'>
+        <Flex fontSize={12} fontWeight={"bold"} gap="2">
           asprogramer
           <Box color={"gray.500"}>• 1w</Box>
         </Flex>
