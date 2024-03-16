961e52496555547d54a85c6a3add5388d205fcda
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-16 18:02:19 +0200

    common changes

--- src/components/FeedPosts/FeedPosts.tsx
+++ src/components/FeedPosts/FeedPosts.tsx
@@ -1,9 +1,9 @@
-import { Container } from "@chakra-ui/react"
+import { Box, Container, Flex, Skeleton, SkeletonCircle, VStack } from "@chakra-ui/react"
 import FeedPost from "./FeedPost"
 import { useEffect, useState } from "react"
 
 const FeedPosts = () => {
-  const [loading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
@@ -12,6 +12,21 @@ const FeedPosts = () => {
   },[])
   return (
     <Container maxW={"container.sm"} py={10} px={2}>
+      {isLoading &&
+				[0, 1, 2].map((_, idx) => (
+					<VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
+						<Flex gap='2'>
+							<SkeletonCircle size='10' />
+							<VStack gap={2} alignItems={"flex-start"}>
+								<Skeleton height='10px' w={"200px"} />
+								<Skeleton height='10px' w={"200px"} />
+							</VStack>
+						</Flex>
+						<Skeleton w={"full"}>
+							<Box h={"400px"}>contents wrapped</Box>
+						</Skeleton>
+					</VStack>
+				))}
         <FeedPost />
         <FeedPost />
         <FeedPost />
