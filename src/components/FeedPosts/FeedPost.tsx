342161ee3f7cbed0160c4bc1bc17d660593cbcb5
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-16 17:53:55 +0200

    common changes

--- src/components/FeedPosts/FeedPost.tsx
+++ src/components/FeedPosts/FeedPost.tsx
@@ -5,11 +5,11 @@ import { Box, Image } from "@chakra-ui/react"
 const FeedPost = () => {
   return (
     <>
-    <PostHeader />
-    <Box my={2} borderRadius={4} overflow={"hidden"}>
-        <Image src="/img2.png" alt="profile pic"/>
-    </Box>
-    <PostFooter />
+      <PostHeader />
+      <Box my={2} borderRadius={4} overflow={"hidden"}>
+        <Image src="/img2.png" alt="profile pic" />
+      </Box>
+      <PostFooter />
     </>
   )
 }
--- src/components/FeedPosts/FeedPosts.tsx
+++ src/components/FeedPosts/FeedPosts.tsx
@@ -1,7 +1,15 @@
 import { Container } from "@chakra-ui/react"
 import FeedPost from "./FeedPost"
+import { useEffect, useState } from "react"
 
 const FeedPosts = () => {
+  const [loading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1200)
+  },[])
   return (
     <Container maxW={"container.sm"} py={10} px={2}>
         <FeedPost />
