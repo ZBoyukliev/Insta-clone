c21d22759d4e576c4d49622a8f8e097c0c4882aa
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-17 16:30:54 +0200

    style suggested header

--- src/components/SuggestedUsers.tsx/SuggestedHeader.tsx
+++ src/components/SuggestedUsers.tsx/SuggestedHeader.tsx
@@ -1,8 +1,30 @@
+import { Avatar, Flex, Link, Text } from "@chakra-ui/react"
+import { Link as RoterLink } from "react-router-dom"
 
 const SuggestedHeader = () => {
-  return (
-    <div>SuggestedHeader</div>
-  )
+    return (
+        <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
+            <Flex alignItems={"center"} gap={2}>
+                <Avatar name="as Zack" size={"lg"} src={"profilepic.png"} />
+                <Text fontSize={12} fontWeight={"bold"}>
+                    as_programer
+                </Text>
+            </Flex>
+            <Link
+                as={RoterLink}
+                to={"/auth"}
+                size={"xs"}
+                background={"transparent"}
+                _hover={{ background: "transparent" }}
+                fontSize={14}
+                fontWeight={"medium"}
+                color={"blue.400"}
+                cursor={"pointer"}
+            >
+                Log out
+            </Link>
+        </Flex>
+    )
 }
 
 export default SuggestedHeader
