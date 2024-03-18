c6a8eb41e65877abc701969d33fa74d26e6b890a
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-18 11:37:01 +0200

    style suggested users

--- src/components/SuggestedUsers.tsx/SuggestedUsers.tsx
+++ src/components/SuggestedUsers.tsx/SuggestedUsers.tsx
@@ -1,12 +1,26 @@
-import { VStack } from "@chakra-ui/react"
+import { Box, Flex, Text, VStack, Link } from "@chakra-ui/react"
 import SuggestedHeader from "./SuggestedHeader"
 
 const SuggestedUsers = () => {
   return (
-	<VStack py={8} px={6} gap={4}>
-        <SuggestedHeader />
+    <VStack py={8} px={6} gap={4}>
+      <SuggestedHeader />
+      <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+        <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+          Suggested for you
+        </Text>
+        <Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
+          See All
+        </Text>
+      </Flex>
+      <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+        © 2024 Built By{"   "}
+        <Link href='https://github.com/ZBoyukliev' target='_blank' color='blue.500' fontSize={14}>
+             Zarko
+        </Link>
+      </Box>
     </VStack>
-    
+
   )
 }
 
