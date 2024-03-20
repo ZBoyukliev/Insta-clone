26d3fc92a4b8e129ff8b750a2a6f01aac7118d77
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-20 11:51:14 +0200

    style the profile header

--- src/components/Profile/ProfileHeader.tsx
+++ src/components/Profile/ProfileHeader.tsx
@@ -1,7 +1,12 @@
+import { Avatar, AvatarGroup, Flex } from "@chakra-ui/react"
 
 const ProfileHeader = () => {
   return (
-    <div>ProfileHeader</div>
+		<Flex gap={{ base: 4, sm: 10 }} py={10} direction={{ base: "column", sm: "row" }}>
+      <AvatarGroup size={{ base: "xl", md: "2xl" }} justifySelf={"center"} alignSelf={"flex-start"} mx={"auto"}>
+				<Avatar src={'/profilepic.png'} />
+			</AvatarGroup>
+    </Flex>
   )
 }
 
