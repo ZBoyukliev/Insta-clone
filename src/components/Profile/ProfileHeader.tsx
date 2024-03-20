4fb86d5c6cd9e3982e1cc1fa2362f63ca3485755
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-20 12:33:42 +0200

    style the profile header

--- src/components/Profile/ProfileHeader.tsx
+++ src/components/Profile/ProfileHeader.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 const ProfileHeader = () => {
 
-  const [ isFollowing, setIsFollowing ] = useState(false);
+  const [isFollowing, setIsFollowing] = useState(false);
 
   return (
     <Flex gap={{ base: 4, sm: 10 }} py={10} direction={{ base: "column", sm: "row" }}>
@@ -18,26 +18,47 @@ const ProfileHeader = () => {
           alignItems={"center"}
           w={"full"}
         >
-					<Text fontSize={{ base: "sm", md: "lg" }}>as_programer</Text>
-          {/* <Flex gap={4} alignItems={"center"} justifyContent={"center"}> */}
-							<Button
-								bg={"white"}
-								color={"black"}
-								_hover={{ bg: "whiteAlpha.800" }}
-								size={{ base: "xs", md: "sm" }}
-							>
-								Edit Profile
-							</Button>
-						{/* </Flex> */}
+          <Text fontSize={{ base: "sm", md: "lg" }}>as_programer</Text>
+          <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
             <Button
-								bg={"blue.500"}
-								color={"white"}
-								_hover={{ bg: "blue.600" }}
-								size={{ base: "xs", md: "sm" }}
-                onClick={() => setIsFollowing(!isFollowing)}
-							>
-								{isFollowing ? "Unfollow" : "Follow"}
-							</Button>
+              bg={"white"}
+              color={"black"}
+              _hover={{ bg: "whiteAlpha.800" }}
+              size={{ base: "xs", md: "sm" }}
+            >
+              Edit Profile
+            </Button>
+          </Flex>
+          <Button
+            bg={"blue.500"}
+            color={"white"}
+            _hover={{ bg: "blue.600" }}
+            size={{ base: "xs", md: "sm" }}
+            onClick={() => setIsFollowing(!isFollowing)}
+          >
+            {isFollowing ? "Unfollow" : "Follow"}
+          </Button>
+
+        </Flex>
+        <Flex alignItems={"center"} gap={{ base: 2, sm: 4 }}>
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as='span' fontWeight={"bold"} mr={1}>
+              {4}
+            </Text>
+            Posts
+          </Text>
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as='span' fontWeight={"bold"} mr={1}>
+              {179}
+            </Text>
+            Followers
+          </Text>
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as='span' fontWeight={"bold"} mr={1}>
+              {15}
+            </Text>
+            Following
+          </Text>
         </Flex>
       </VStack>
     </Flex>
