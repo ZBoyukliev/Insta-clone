4fd367e0c094d27aaa8c8695bc0a485221f5ef7e
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-22 11:43:20 +0200

    add modal to the profile post

--- src/components/Profile/ProfilePost.tsx
+++ src/components/Profile/ProfilePost.tsx
@@ -1,4 +1,4 @@
-import { Flex, GridItem, Image, Text } from "@chakra-ui/react";
+import { Avatar, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
 import { AiFillHeart } from "react-icons/ai";
 import { FaComment } from "react-icons/fa";
 
@@ -7,7 +7,11 @@ interface ProfilePostProps {
 };
 
 const ProfilePost = ({img}: ProfilePostProps) => {
+
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
+	<>
     <GridItem
 				cursor={"pointer"}
 				borderRadius={4}
@@ -16,6 +20,7 @@ const ProfilePost = ({img}: ProfilePostProps) => {
 				borderColor={"whiteAlpha.300"}
 				position={"relative"}
 				aspectRatio={1 / 1}
+				onClick={onOpen}
 			>
 				<Flex
 					opacity={0}
@@ -49,6 +54,46 @@ const ProfilePost = ({img}: ProfilePostProps) => {
 
 				<Image src={img} alt='profile post' w={"100%"} h={"100%"} objectFit={"cover"} />
 			</GridItem>
+			<Modal isOpen={isOpen} onClose={onClose} isCentered={true} size={{ base: "3xl", md: "5xl" }}>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalCloseButton />
+					<ModalBody bg={"black"} pb={5}>
+						<Flex
+							gap='4'
+							w={{ base: "90%", sm: "70%", md: "full" }}
+							mx={"auto"}
+							maxH={"90vh"}
+							minH={"50vh"}
+						>
+							<Flex
+								borderRadius={4}
+								overflow={"hidden"}
+								border={"1px solid"}
+								borderColor={"whiteAlpha.300"}
+								flex={1.5}
+								justifyContent={"center"}
+								alignItems={"center"}
+							>
+								<Image src={img} alt='profile post' />
+							</Flex>
+							<Flex flex={1} flexDir={"column"} px={10} display={{ base: "none", md: "flex" }}>
+								<Flex alignItems={"center"} justifyContent={"space-between"}>
+									<Flex alignItems={"center"} gap={4}>
+										<Avatar src={img} size={"sm"} name='As a Programmer' />
+										<Text fontWeight={"bold"} fontSize={12}>
+											username
+										</Text>
+									</Flex>
+
+								</Flex>
+
+							</Flex>
+						</Flex>
+					</ModalBody>
+				</ModalContent>
+			</Modal>
+			</>
   )
 }
 
