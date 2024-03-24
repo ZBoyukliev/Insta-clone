1674df1e78be192b172ce89023aca7617bf00de8
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-24 18:02:27 +0200

    style notifications

--- src/components/Sidebar/items/Notifications.tsx
+++ src/components/Sidebar/items/Notifications.tsx
@@ -1,7 +1,10 @@
-import { Box, Flex, Tooltip } from "@chakra-ui/react";
+import { Box, Flex, Tooltip, useColorMode } from "@chakra-ui/react";
 import { NotificationsLogo } from "../../../assets/constants";
+import { BiBell } from "react-icons/bi";
 
 const Notifications = () => {
+	const { colorMode } = useColorMode();
+
 	return (
 		<Tooltip
 			hasArrow
@@ -14,13 +17,14 @@ const Notifications = () => {
 			<Flex
 				alignItems={"center"}
 				gap={4}
-				_hover={{ bg: "whiteAlpha.400" }}
+				_hover={{ bg: colorMode === "light" ? "blackAlpha.400" : "whiteAlpha.400" }}
 				borderRadius={6}
 				p={2}
 				w={{ base: 10, md: "full" }}
 				justifyContent={{ base: "center", md: "flex-start" }}
+				bg={colorMode === "light" ? "#f1f4f7" : "black"}
 			>
-				<NotificationsLogo />
+				{colorMode === "light" ? <BiBell size={20} /> : <NotificationsLogo />}
 				<Box display={{ base: "none", md: "block" }}>Notifications</Box>
 			</Flex>
 		</Tooltip>
