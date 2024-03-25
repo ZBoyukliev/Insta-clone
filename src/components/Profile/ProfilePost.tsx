1ff0424868eb0939b855d73ff5808c528a890ecc
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-25 12:24:11 +0200

    create comment component

--- src/components/Profile/ProfilePost.tsx
+++ src/components/Profile/ProfilePost.tsx
@@ -1,109 +1,175 @@
-import { Avatar, Box, Divider, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalOverlay, Text, useColorMode, useDisclosure } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Flex,
+  GridItem,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  Text,
+  VStack,
+  useColorMode,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { AiFillHeart } from "react-icons/ai";
 import { FaComment } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import Comment from "../Comment/Comment";
 
 interface ProfilePostProps {
-	img: string;
-};
-
-const ProfilePost = ({ img }: ProfilePostProps) => {
-
-	const { isOpen, onOpen, onClose } = useDisclosure();
-	const { colorMode } = useColorMode()
-
-	return (
-		<>
-			<GridItem
-				cursor={"pointer"}
-				borderRadius={4}
-				overflow={"hidden"}
-				border={"1px solid"}
-				borderColor={"whiteAlpha.300"}
-				position={"relative"}
-				aspectRatio={1 / 1}
-				onClick={onOpen}
-			>
-				<Flex
-					opacity={0}
-					_hover={{ opacity: 1 }}
-					position={"absolute"}
-					top={0}
-					left={0}
-					right={0}
-					bottom={0}
-					bg={"blackAlpha.700"}
-					transition={"all 0.3s ease"}
-					zIndex={1}
-					justifyContent={"center"}
-				>
-					<Flex alignItems={"center"} justifyContent={"center"} gap={50}>
-						<Flex>
-							<AiFillHeart size={20} />
-							<Text fontWeight={"bold"} ml={2}>
-								7
-							</Text>
-						</Flex>
-
-						<Flex>
-							<FaComment size={20} />
-							<Text fontWeight={"bold"} ml={2}>
-								5
-							</Text>
-						</Flex>
-					</Flex>
-				</Flex>
-
-				<Image src={img} alt='profile post' w={"100%"} h={"100%"} objectFit={"cover"} />
-			</GridItem>
-			<Modal isOpen={isOpen} onClose={onClose} isCentered={true} size={{ base: "3xl", md: "5xl" }}>
-				<ModalOverlay />
-				<ModalContent>
-					<ModalCloseButton />
-					<ModalBody bg={colorMode === "light" ? "white" : "black"} pb={5}>
-						<Flex
-							gap='4'
-							w={{ base: "90%", sm: "70%", md: "full" }}
-							mx={"auto"}
-							maxH={"90vh"}
-							minH={"50vh"}
-						>
-							<Flex
-								borderRadius={4}
-								overflow={"hidden"}
-								border={"1px solid"}
-								borderColor={"whiteAlpha.300"}
-								flex={1.5}
-								justifyContent={"center"}
-								alignItems={"center"}
-							>
-								<Image src={img} alt='profile post' />
-							</Flex>
-							<Flex flex={1} flexDir={"column"} px={10} display={{ base: "none", md: "flex" }}>
-								<Flex alignItems={"center"} justifyContent={"space-between"}>
-									<Flex alignItems={"center"} gap={4}>
-										<Avatar src={"profilepic.png"} size={"sm"} name='As a Programmer' />
-										<Text fontWeight={"bold"} fontSize={12}>
-											username
-										</Text>
-									</Flex>
-									<Box
-										bg={"transparent"}
-										_hover={{ bg: "whiteAlpha.300", color: "red.600" }}
-										borderRadius={4}
-										p={1}
-									>
-										<MdDelete size={20} cursor='pointer' />
-									</Box>
-								</Flex>
-								<Divider my={4} bg={"gray.500"} />
-							</Flex>
-						</Flex>
-					</ModalBody>
-				</ModalContent>
-			</Modal>
-		</>
-	)
+  img: string;
 }
 
-export default ProfilePost
+const ProfilePost = ({ img }: ProfilePostProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode } = useColorMode();
+
+  return (
+    <>
+      <GridItem
+        cursor={"pointer"}
+        borderRadius={4}
+        overflow={"hidden"}
+        border={"1px solid"}
+        borderColor={"whiteAlpha.300"}
+        position={"relative"}
+        aspectRatio={1 / 1}
+        onClick={onOpen}
+      >
+        <Flex
+          opacity={0}
+          _hover={{ opacity: 1 }}
+          position={"absolute"}
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          bg={"blackAlpha.700"}
+          transition={"all 0.3s ease"}
+          zIndex={1}
+          justifyContent={"center"}
+        >
+          <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
+            <Flex>
+              <AiFillHeart size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                7
+              </Text>
+            </Flex>
+
+            <Flex>
+              <FaComment size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                5
+              </Text>
+            </Flex>
+          </Flex>
+        </Flex>
+
+        <Image
+          src={img}
+          alt="profile post"
+          w={"100%"}
+          h={"100%"}
+          objectFit={"cover"}
+        />
+      </GridItem>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered={true}
+        size={{ base: "3xl", md: "5xl" }}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody bg={colorMode === "light" ? "white" : "black"} pb={5}>
+            <Flex
+              gap="4"
+              w={{ base: "90%", sm: "70%", md: "full" }}
+              mx={"auto"}
+              maxH={"90vh"}
+              minH={"50vh"}
+            >
+              <Flex
+                borderRadius={4}
+                overflow={"hidden"}
+                border={"1px solid"}
+                borderColor={"whiteAlpha.300"}
+                flex={1.5}
+                justifyContent={"center"}
+                alignItems={"center"}
+              >
+                <Image src={img} alt="profile post" />
+              </Flex>
+              <Flex
+                flex={1}
+                flexDir={"column"}
+                px={10}
+                display={{ base: "none", md: "flex" }}
+              >
+                <Flex alignItems={"center"} justifyContent={"space-between"}>
+                  <Flex alignItems={"center"} gap={4}>
+                    <Avatar
+                      src={"profilepic.png"}
+                      size={"sm"}
+                      name="As a Programmer"
+                    />
+                    <Text fontWeight={"bold"} fontSize={12}>
+                      username
+                    </Text>
+                  </Flex>
+                  <Box
+                    bg={"transparent"}
+                    _hover={{ bg: "whiteAlpha.300", color: "red.600" }}
+                    borderRadius={4}
+                    p={1}
+                  >
+                    <MdDelete size={20} cursor="pointer" />
+                  </Box>
+                </Flex>
+                <Divider my={4} bg={"gray.500"} />
+                <VStack
+                  w={"full"}
+                  alignItems={"start"}
+                  maxH={"350px"}
+                  overflowY={"auto"}
+				  flexDirection={"column"}
+                >
+
+                  {/* COMMENTS */}
+                  <Comment
+                    createdAt="1h ago"
+                    username="asprogramer_"
+                    profilepic="/profilepic.png"
+                    text="Dummy images"
+                  />
+                  <Comment
+                    createdAt="2h ago"
+                    username="asprogramer88"
+                    profilepic="/profilepic.png"
+                    text="Dummy images"
+                  />
+                  <Comment
+                    createdAt="3h ago"
+                    username="asprogramer98"
+                    profilepic="/profilepic.png"
+                    text="Dummy images"
+                  />
+                </VStack>
+                {/* <Divider my={4} bg={"gray.500"} /> */}
+              </Flex>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ProfilePost;
