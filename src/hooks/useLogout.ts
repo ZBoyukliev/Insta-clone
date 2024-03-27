bee698212f47a3d353738abcdf43cab7ac3bd93f
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-27 12:44:58 +0200

    logout functionality changes

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,19 +1,19 @@
 import { useSignOut } from "react-firebase-hooks/auth";
 import { auth } from "../firebase/firebase";
 import useShowToast from "./useShowToast";
-// import useAuthStore from "../store/authStore";
+import useAuthStore from "../store/authStore";
 
 const useLogout = () => {
 	const [signOut, isLoggingOut, error] = useSignOut(auth);
 	const showToast = useShowToast();
-	// const logoutUser = useAuthStore((state) => state.logout);
+	const logoutUser = useAuthStore((state) => (state as { logout: () => void }).logout);
 
 	const handleLogout = async () => {
 		try {
 			await signOut();
 			localStorage.removeItem("user-info");
             console.log('Logged out')
-			// logoutUser();
+			logoutUser();
 		} catch (error) {
 			const errorMessage = (error as { message: string })?.message || "An unknown error occurred";
 			showToast("Error", errorMessage, "error");
