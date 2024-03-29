2ffe2f3254f83ddcf87f426f75311fb1f8d995d3
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-03-29 11:44:04 +0200

    add prefix in google auth

--- src/components/AuthForm/GoogleAuth.tsx
+++ src/components/AuthForm/GoogleAuth.tsx
@@ -1,18 +1,18 @@
 import { Flex, Image, Text } from "@chakra-ui/react";
 
-const GoogleAuth = () => {
-    return (
-        <Flex
-            alignItems={"center"}
-            justifyContent={"center"}
-            cursor={"pointer"}
-          >
-            <Image src="/google.png" h={5} alt="Google logo" />
-            <Text mx={2} color={"blue.400"}>
-              Login with Google
-            </Text>
-          </Flex>
-    );
+interface GoogleAuthProps {
+  prefix: string;
+}
+
+const GoogleAuth = ({ prefix }: GoogleAuthProps) => {
+  return (
+    <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}>
+      <Image src="/google.png" h={5} alt="Google logo" />
+      <Text mx={2} color={"blue.400"}>
+        {prefix} with Google
+      </Text>
+    </Flex>
+  );
 };
 
 export default GoogleAuth;
