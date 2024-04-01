da8e3ec86ab021bbe3e9ca6b3da084b2230baa06
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-04-01 19:07:15 +0300

    create user profile store

---
+++ src/store/userProfileStore.ts
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+import { User } from "./authStore";
+
+interface Post {
+  id: string;
+}
+
+const useUserProfileStore = create((set) => ({
+	userProfile: null as User | null,
+	setUserProfile: (userProfile: User) => set({ userProfile }),
+	// this is used to update the number of posts in the profile page
+	addPost: (post: Post) =>
+		set((state: { userProfile: User }) => ({
+			userProfile: { 
+                ...state.userProfile, 
+                posts: state.userProfile.posts ? [post.id, ...state.userProfile.posts] : [post.id],
+            },
+		})),
+	deletePost: (postId: string) =>
+		set((state: { userProfile: User }) => ({
+			userProfile: {
+				...state.userProfile,
+				posts: state.userProfile.posts ? state.userProfile.posts.filter((id: string) => id !== postId) : [],
+			},
+		})),
+}));
+
+export default useUserProfileStore;
