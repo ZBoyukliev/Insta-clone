b2a11b2a650e8dbdf2a6587fe5a26554cadccd1e
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-04-02 15:25:07 +0300

    create hook get user profile

---
+++ src/hooks/useGetUserProfileByUsername.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import useShowToast from "./useShowToast";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { firestore } from "../firebase/firebase";
+import useUserProfileStore from "../store/userProfileStore";
+import { User } from "../store/authStore"; // Assuming the User type is defined here similar to file_context_3
+
+const useGetUserProfileByUsername = (username: string) => {
+	const [isLoading, setIsLoading] = useState(true);
+	const showToast = useShowToast();
+	const userProfileStore = useUserProfileStore() as {
+		userProfile: User | null;
+		setUserProfile: (userProfile: User | null) => void;
+	};
+	const userProfile: User | null = userProfileStore.userProfile;
+	const setUserProfile = userProfileStore.setUserProfile;
+
+	useEffect(() => {
+		const getUserProfile = async () => {
+			setIsLoading(true);
+			try {
+				const q = query(collection(firestore, "users"), where("username", "==", username));
+				const querySnapshot = await getDocs(q);
+
+				if (querySnapshot.empty) return setUserProfile(null);
+
+				let userDoc: User | null = null;
+				querySnapshot.forEach((doc) => {
+					userDoc = doc.data() as User;
+				});
+
+				setUserProfile(userDoc);
+				console.log(userDoc);
+			} catch (error) {
+				const errorMessage = (error as Error).message;
+				showToast("Error", errorMessage, "error");
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		getUserProfile();
+	}, [setUserProfile, username, showToast]);
+
+	return { isLoading, userProfile };
+};
+
+export default useGetUserProfileByUsername;
--- src/hooks/useShowToast.ts
+++ src/hooks/useShowToast.ts
@@ -1,11 +1,16 @@
 import { useToast } from "@chakra-ui/react";
+import { useCallback } from "react";
 
 const useShowToast = () => {
 	const toast = useToast();
 
 	// useCallback is used to prevent infinite loop, by  caching the function
-	const showToast = (
-		(title: string, description: string, status: "info" | "warning" | "success" | "error") => {
+	const showToast = useCallback(
+		(
+			title: string,
+			description: string,
+			status: "info" | "warning" | "success" | "error"
+		) => {
 			toast({
 				title: title,
 				description: description,
@@ -13,7 +18,8 @@ const useShowToast = () => {
 				duration: 3000,
 				isClosable: true,
 			});
-		}
+		},
+		[toast]
 	);
 
 	return showToast;
