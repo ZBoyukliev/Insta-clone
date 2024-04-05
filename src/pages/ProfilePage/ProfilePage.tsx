124edd44eeb5aefba9bb86b542e5606d7c5bde10
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-04-05 09:16:03 +0300

    remove all the console logs

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -12,7 +12,6 @@ const ProfilePage = () => {
 
 	const userNotFound = !isLoading && !userProfile;
 	if (userNotFound) return <UserNotFound />;
-	console.log('userProfile', userProfile)
 	return (
 		<Container maxW='container.lg' py={5}>
 			<Flex py={10} px={4} pl={{ base: 4, md: 10 }} w={"full"} mx={"auto"} flexDirection={"column"}>
