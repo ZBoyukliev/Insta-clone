8e0d606bbdc1b541883b06ca3f2a385dbf64a7f5
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-04-15 09:42:14 +0300

    create use like post hook

--- src/components/FeedPosts/PostFooter.tsx
+++ src/components/FeedPosts/PostFooter.tsx
@@ -1,78 +1,173 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Text,
-} from "@chakra-ui/react";
-import { CommentLogo, NotificationsLogo } from "../../assets/constants";
-import { PostFooterProps } from "./FeedPost";
+import { Box, Button, Flex, Input, InputGroup, InputRightElement, Text, useDisclosure } from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import { CommentLogo, NotificationsLogo, UnlikeLogo } from "../../assets/constants";
+import usePostComment from "../../hooks/usePostComment";
+import useAuthStore from "../../store/authStore";
+import useLikePost from "../../hooks/useLikePost";
+import { timeAgo } from "../../utils/timeAgo";
+import CommentsModal from "../Modals/CommentsModal";
 
-const PostFooter = ({ isProfilePage }: PostFooterProps) => {
-  return (
-    <Box mb={10} marginTop={"auto"}>
-      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
-        <Box cursor={"pointer"} fontSize={18}>
-          <NotificationsLogo />
-          {/* <UnlikeLogo /> */}
-        </Box>
+const PostFooter = ({ post, isProfilePage, creatorProfile }) => {
+	const { isCommenting, handlePostComment } = usePostComment();
+	const [comment, setComment] = useState("");
+	const authUser = useAuthStore((state: any) => state.user);
+	const commentRef = useRef(null);
+	const { handleLikePost, isLiked, likes } = useLikePost(post);
+	const { isOpen, onOpen, onClose } = useDisclosure();
 
-        <Box cursor={"pointer"} fontSize={18}>
-          <CommentLogo />
-        </Box>
-      </Flex>
-      <Text fontWeight={600} fontSize={"sm"}>
-        likes
-      </Text>
+	const handleSubmitComment = async () => {
+		await handlePostComment(post.id, comment);
+		setComment("");
+	};
 
-      <Text fontSize="12" color={"gray"}>
-        Posted time ago
-      </Text>
-      {!isProfilePage && (
-        <>
-          <Text fontSize="sm" fontWeight={700}>
-            poster{" "}
-            <Text as="span" fontWeight={400}>
-              feeling amazing
-            </Text>
-          </Text>
-          <Text fontSize="sm" color={"gray"} cursor={"pointer"}>
-            View all 56 comments
-          </Text>
-          {/* COMMENTS MODAL ONLY IN THE HOME PAGE */}
-        </>
-      )}
+	return (
+		<Box mb={10} marginTop={"auto"}>
+			<Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
+				<Box onClick={handleLikePost} cursor={"pointer"} fontSize={18}>
+					{!isLiked ? <NotificationsLogo /> : <UnlikeLogo />}
+				</Box>
 
-      <Flex
-        alignItems={"center"}
-        gap={2}
-        justifyContent={"space-between"}
-        w={"full"}
-      >
-        <InputGroup>
-          <Input
-            variant={"flushed"}
-            placeholder={"Add a comment..."}
-            fontSize={14}
-          />
-          <InputRightElement>
-            <Button
-              fontSize={14}
-              color={"blue.400"}
-              fontWeight={600}
-              cursor={"pointer"}
-              _hover={{ color: "white" }}
-              bg={"transparent"}
-            >
-              Post
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-      </Flex>
-    </Box>
-  );
+				<Box cursor={"pointer"} fontSize={18} onClick={() => commentRef.current.focus()}>
+					<CommentLogo />
+				</Box>
+			</Flex>
+			<Text fontWeight={600} fontSize={"sm"}>
+				{likes} likes
+			</Text>
+
+			{isProfilePage && (
+				<Text fontSize='12' color={"gray"}>
+					Posted {timeAgo(post.createdAt)}
+				</Text>
+			)}
+
+			{!isProfilePage && (
+				<>
+					<Text fontSize='sm' fontWeight={700}>
+						{creatorProfile?.username}{" "}
+						<Text as='span' fontWeight={400}>
+							{post.caption}
+						</Text>
+					</Text>
+					{post.comments.length > 0 && (
+						<Text fontSize='sm' color={"gray"} cursor={"pointer"} onClick={onOpen}>
+							View all {post.comments.length} comments
+						</Text>
+					)}
+					{/* COMMENTS MODAL ONLY IN THE HOME PAGE */}
+					{isOpen ? <CommentsModal isOpen={isOpen} onClose={onClose} post={post} /> : null}
+				</>
+			)}
+
+			{authUser && (
+				<Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"}>
+					<InputGroup>
+						<Input
+							variant={"flushed"}
+							placeholder={"Add a comment..."}
+							fontSize={14}
+							onChange={(e) => setComment(e.target.value)}
+							value={comment}
+							ref={commentRef}
+						/>
+						<InputRightElement>
+							<Button
+								fontSize={14}
+								color={"blue.500"}
+								fontWeight={600}
+								cursor={"pointer"}
+								_hover={{ color: "white" }}
+								bg={"transparent"}
+								onClick={handleSubmitComment}
+								isLoading={isCommenting}
+							>
+								Post
+							</Button>
+						</InputRightElement>
+					</InputGroup>
+				</Flex>
+			)}
+		</Box>
+	);
 };
 
 export default PostFooter;
+
+// import {
+//   Box,
+//   Button,
+//   Flex,
+//   Input,
+//   InputGroup,
+//   InputRightElement,
+//   Text,
+// } from "@chakra-ui/react";
+// import { CommentLogo, NotificationsLogo } from "../../assets/constants";
+// import { PostFooterProps } from "./FeedPost";
+
+// const PostFooter = ({ isProfilePage }: PostFooterProps) => {
+//   return (
+//     <Box mb={10} marginTop={"auto"}>
+//       <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
+//         <Box cursor={"pointer"} fontSize={18}>
+//           <NotificationsLogo />
+//           {/* <UnlikeLogo /> */}
+//         </Box>
+
+//         <Box cursor={"pointer"} fontSize={18}>
+//           <CommentLogo />
+//         </Box>
+//       </Flex>
+//       <Text fontWeight={600} fontSize={"sm"}>
+//         likes
+//       </Text>
+
+//       <Text fontSize="12" color={"gray"}>
+//         Posted time ago
+//       </Text>
+//       {!isProfilePage && (
+//         <>
+//           <Text fontSize="sm" fontWeight={700}>
+//             poster{" "}
+//             <Text as="span" fontWeight={400}>
+//               feeling amazing
+//             </Text>
+//           </Text>
+//           <Text fontSize="sm" color={"gray"} cursor={"pointer"}>
+//             View all 56 comments
+//           </Text>
+//           {/* COMMENTS MODAL ONLY IN THE HOME PAGE */}
+//         </>
+//       )}
+
+//       <Flex
+//         alignItems={"center"}
+//         gap={2}
+//         justifyContent={"space-between"}
+//         w={"full"}
+//       >
+//         <InputGroup>
+//           <Input
+//             variant={"flushed"}
+//             placeholder={"Add a comment..."}
+//             fontSize={14}
+//           />
+//           <InputRightElement>
+//             <Button
+//               fontSize={14}
+//               color={"blue.400"}
+//               fontWeight={600}
+//               cursor={"pointer"}
+//               _hover={{ color: "white" }}
+//               bg={"transparent"}
+//             >
+//               Post
+//             </Button>
+//           </InputRightElement>
+//         </InputGroup>
+//       </Flex>
+//     </Box>
+//   );
+// };
+
+// export default PostFooter;
