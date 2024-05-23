81d9ea46cc506c8061eab94e7733b9b2d5053999
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-05-23 12:38:06 +0300

    common changes

--- src/store/postStore.ts
+++ src/store/postStore.ts
@@ -2,7 +2,7 @@ import { create, StateCreator } from "zustand";
 
 interface Post {
     id: string;
-    [key: string]: any; // Allow any additional properties
+    [key: string]: any; 
 }
 
 interface PostStore {
@@ -33,40 +33,3 @@ const usePostStore: StateCreator<PostStore> = (set) => ({
 });
 
 export default create(usePostStore);
-
-
-// import { create, SetState  } from "zustand";
-
-// interface Post {
-//     id: string;
-//     [key: string]: any;
-// }
-
-// interface PostStore {
-//     posts: Post[];
-//     createPost: (post: any) => void;
-//     deletePost: (id: any) => void;
-//     setPosts: (posts: any) => void;
-//     addComment: (postId: any, comment: any) => void;
-// }
-
-// const usePostStore = create<PostStore>((set: SetState<PostStore>) => ({
-//     posts: [],
-//     createPost: (post: any) => set((state: any) => ({ posts: [post, ...state.posts] })),
-//     deletePost: (id: any) => set((state: any) => ({ posts: state.posts.filter((post: any) => post.id !== id) })),
-//     setPosts: (posts: any) => set({ posts }),
-//     addComment: (postId: any, comment: any) =>
-//         set((state: any) => ({
-//             posts: state.posts.map((post: any) => {
-//                 if (post.id === postId) {
-//                     return {
-//                         ...post,
-//                         comments: [...post.comments, comment],
-//                     };
-//                 }
-//                 return post;
-//             }),
-//         })),
-// }));
-
-// export default usePostStore;
